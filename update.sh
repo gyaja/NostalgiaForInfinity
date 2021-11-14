e578e6acc694150e8340a649327ce89a9dc412ee
gyaja <33547907+gyaja@users.noreply.github.com>
2021-11-14 19:42:24 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /root/NostalgiaForInfinity
+
+GITRESPONSE=`git pull`
+UPDATED='Already up to date.'
+
+if [[ $GITRESPONSE != $UPDATED ]]; then
+        cp NostalgiaForInfinityX.py /root/NostalgiaForInfinity/user_data/strategies/NostalgiaForInfinityX.py
+                GITMESSAGE=`git log HEAD@{1}.. --pretty=%B`
+        cd /root/NostalgiaForInfinity
+        docker-compose down
+        docker-compose up --force-recreate --build -d
+
+        if [[ $GITMESSAGE != "" ]]; then
+                python3 /root/NostalgiaForInfinity/notify.py "$GITMESSAGE"
+        else
+                python3 /root/NostalgiaForInfinity/notify.py
+        fi
+
+fi
